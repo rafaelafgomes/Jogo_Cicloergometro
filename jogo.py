da8d978d29918ca18bde25db734531271d91e1c7
RAFAELA FREITAS GOMES <rafaela.gomes.2024000322@estudante.ifsudestemg.edu.br>
2026-06-02 22:25:46 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import requests
 import io
+import math
 
 pygame.init()
 
@@ -27,27 +28,37 @@ vel_max = 5
 config_opcao = 0
 
 fases = [
-    {"nome": "Mercúrio", "duracao": 90},
-    {"nome": "Vênus", "duracao": 120},
-    {"nome": "Marte", "duracao": 180},
-    {"nome": "Saturno", "duracao": 240}
+    {"nome": "Mercúrio", "distancia": 30},
+    {"nome": "Vênus", "distancia": 30},
+    {"nome": "Marte", "distancia": 30},
+    {"nome": "Saturno", "distancia": 30}
 ]
 
 fase_atual = 0
-duracao_missao = fases[fase_atual]["duracao"]
+distancia_total = fases[fase_atual]["distancia"]
+distancia_restante = distancia_total
 
 # ============Jogo============================
 velocidade = 0
-progresso = 0
+progresso = 0 #porcentagem da distancia percorrida
 pontuacao = 0
 
 tempo_inicio = pygame.time.get_ticks()
 tempo_pontos = pygame.time.get_ticks()
+ultimo_tempo = pygame.time.get_ticks()
 
-nave_x = 200
+nave_x_inicial = 120
+nave_x_final = 520
+nave_x = nave_x_inicial
 nave_y = 300
 
-planeta_x = 900
+#flutuação da nave
+nave_y_base = 300
+nave_y = nave_y_base
+vel_nave_y = 0
+tempo_mudar_movimento = pygame.time.get_ticks()
+
+planeta_x = 650
 planeta_y = 300
 
 #================Barras=======================
@@ -96,10 +107,10 @@ def carregar_imagem_url(url):
     imagem_bytes = io.BytesIO(resposta.content)
     return pygame.image.load(imagem_bytes).convert_alpha()
 
-mercurio_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/mercurio.png"),(80,80))
-venus_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/venus.png"),(80,80))
-marte_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/marte.png"),(80,80))
-saturno_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/saturno.png"),(100,80))
+mercurio_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/mercurio.png"),(220,220))
+venus_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/venus.png"),(240,240))
+marte_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/marte.png"),(230,230))
+saturno_img = pygame.transform.scale(carregar_imagem_url("https://raw.githubusercontent.com/rafaelafgomes/Jogo_Cicloergometro/main/imagens/saturno.png"),(320,220))
 
 imagens_planetas = {
     "Mercúrio": mercurio_img,
@@ -151,7 +162,13 @@ while rodando:
             progresso = 0
             velocidade = 0
             pontuacao = 0
+            
+            distancia_total = fases[fase_atual]["distancia"]
+            distancia_restante = distancia_total
+            
             tempo_inicio = pygame.time.get_ticks()
+            ultimo_tempo = pygame.time.get_ticks()
+            
             estado = JOGO
         if clicou(300,280,200,50):
             estado = CONFIGURACAO
@@ -232,8 +249,7 @@ while rodando:
     
     #====================Jogo============================
     elif estado == JOGO:
-    
-        duracao_missao = fases[fase_atual]["duracao"]
+                
         teclas = pygame.key.get_pressed()
 
         # controle (simulação pedal)
@@ -242,6 +258,27 @@ while rodando:
         if teclas[pygame.K_DOWN]:
             velocidade -= 0.2
 
+        # movimento suave da nave (efeito flutuar)
+        tempo_atual = pygame.time.get_ticks()
+
+        # muda o comportamento só de vez em quando
+        if tempo_atual - tempo_mudar_movimento > random.randint(1200, 2500):
+            vel_nave_y = random.uniform(-0.25, 0.25)
+            tempo_mudar_movimento = tempo_atual
+
+        # aplica movimento vertical na nave
+        nave_y += vel_nave_y
+
+        # limita o quanto sobe/desce a nave
+        if nave_y < nave_y_base - 8:
+            nave_y = nave_y_base - 8
+            vel_nave_y *= -1
+
+        if nave_y > nave_y_base + 8:
+            nave_y = nave_y_base + 8
+            vel_nave_y *= -1
+        
+        
         velocidade *= 0.98
         
         velocidade = max(0, min(velocidade, vel_max))
@@ -264,12 +301,20 @@ while rodando:
                 pontuacao += 20
             tempo_pontos = tempo_atual
         
-        # progresso
-        vel_media_ideal = (vel_ideal_min + vel_ideal_max) / 2
-        tempo_passado = (pygame.time.get_ticks() - tempo_inicio) / 1000
-        progresso = tempo_passado / duracao_missao
-        progresso = min(progresso, 1)
-        planeta_x = 800 - (progresso * 600)
+        # progresso baseado em distância
+        tempo_atual = pygame.time.get_ticks()
+        delta_tempo = (tempo_atual - ultimo_tempo) / 1000
+        ultimo_tempo = tempo_atual
+        # reduz distância conforme pedalada
+        distancia_restante -= velocidade * delta_tempo
+        # impede valor negativo
+        distancia_restante = max(0, distancia_restante)
+        # porcentagem concluída
+        progresso = 1 - (distancia_restante / distancia_total)
+        # move planeta visualmente
+        nave_x = nave_x_inicial + (
+        progresso * (nave_x_final - nave_x_inicial)
+        )
         
         # fundo gradiente
         for y in range(600):
@@ -294,24 +339,21 @@ while rodando:
                     linha[1] = random.randint(0,600)
 
                 pygame.draw.line(tela,(255,255,255),(linha[0],linha[1]),(linha[0]+linha[2],linha[1]),2)
-        
-        # nave
-        if velocidade > vel_ideal_max:
-            tamanho = random.randint(15,25)
-            pygame.draw.polygon(tela, (255, random.randint(100,200), 0), [
-                (nave_x, nave_y + 15),
-                (nave_x - 20, nave_y + 10),
-                (nave_x - 20, nave_y + 20)
-            ])
-        tela.blit(nave_img, (nave_x, nave_y))
+      
         
         #planeta
         nome_planeta = fases[fase_atual]["nome"]
         img = imagens_planetas[nome_planeta]
-        if nome_planeta == "Saturno":
-            tela.blit(img, (planeta_x - 50, planeta_y - 40))
-        else:
-            tela.blit(img, (planeta_x - 40, planeta_y - 40))
+        largura = img.get_width()
+        altura = img.get_height()
+
+        tela.blit(img, (
+            planeta_x - largura // 2,
+            planeta_y - altura // 2
+        ))
+        
+        # nave
+        tela.blit(nave_img, (nave_x, nave_y))
         
         # tempo
         tempo_seg = (pygame.time.get_ticks() - tempo_inicio)//1000
@@ -349,9 +391,10 @@ while rodando:
         pygame.draw.rect(tela, (255,255,255), (barra_x, barra_y, barra_largura, barra_altura), 2)
     
         # aviso
-        if progresso > 0.9:
-            tela.blit(fonte.render("Diminua a velocidade!", True,(255,255,0)), (250,200))
+        if progresso > 0.7:
+            tela.blit(fonte.render("Preparando pouso: reduza a velocidade", True,(255,255,0)), (180,200))
 
+        # colisão visual com o planeta
         if progresso >= 1:
             estado = FIM
             
@@ -366,7 +409,11 @@ while rodando:
             progresso = 0
             velocidade = 0
             pontuacao = 0
+            distancia_total = fases[fase_atual]["distancia"]
+            distancia_restante = distancia_total
             tempo_inicio = pygame.time.get_ticks()
+            ultimo_tempo = pygame.time.get_ticks()
+            nave_x = nave_x_inicial
             estado = JOGO
             pygame.time.delay(200)
 
